0ab9523245d5a6902126000fe67569c40a3473ef
Leonardo <leonardocr.ti21@utsjr.edu.mx>
2025-06-10 17:19:37 -0600

    Actualizacion de redes

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -5,20 +5,14 @@ export default function Contacto() {
   return (
     <section id="contacto" className="contacto-section">
       <h2>Contáctanos</h2>
-      <p>¡Síguenos en nuestras redes sociales o envíanos un mensaje directo!</p>
+      <p>¡Envíanos un mensaje directo!</p>
 
       <div className="redes-sociales">
-        <a href="https://wa.me/5215512345678" target="_blank" rel="noopener noreferrer" className="icono whatsapp">
+        <a href="https://wa.me/524272259083" target="_blank" rel="noopener noreferrer" className="icono whatsapp">
           <FaWhatsapp />
         </a>
-        <a href="https://www.facebook.com/TuPagina" target="_blank" rel="noopener noreferrer" className="icono facebook">
+        <a href="https://www.facebook.com/https://www.facebook.com/leonardo.chavero.522?mibextid=LQQJ4d" target="_blank" rel="noopener noreferrer" className="icono facebook">
           <FaFacebookF />
-        </a>
-        <a href="https://www.instagram.com/TuPerfil" target="_blank" rel="noopener noreferrer" className="icono instagram">
-          <FaInstagram />
-        </a>
-        <a href="https://www.tiktok.com/@TuUsuario" target="_blank" rel="noopener noreferrer" className="icono tiktok">
-          <FaTiktok />
         </a>
       </div>
     </section>
