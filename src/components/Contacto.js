f8c63ccf0a9ad3e71838a4d464469005afb14e31
Leonardo <leonardocr.ti21@utsjr.edu.mx>
2025-06-10 17:23:47 -0600

    act numero

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -8,9 +8,15 @@ export default function Contacto() {
       <p>¡Envíanos un mensaje directo!</p>
 
       <div className="redes-sociales">
-        <a href="https://wa.me/524272259083" target="_blank" rel="noopener noreferrer" className="icono whatsapp">
+        <a
+          href="https://wa.me/524272259083?text=Hola%2C%20necesito%20informaci%C3%B3n%20acerca%20de%20un%20producto%20de%20tu%20tienda%2C%20%C2%BFPuedes%20ayudarme%3F"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icono whatsapp"
+        >
           <FaWhatsapp />
         </a>
+
         <a href="https://www.facebook.com/https://www.facebook.com/leonardo.chavero.522?mibextid=LQQJ4d" target="_blank" rel="noopener noreferrer" className="icono facebook">
           <FaFacebookF />
         </a>
